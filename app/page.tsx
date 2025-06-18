497bb414f1bea07a5c31087502dcd1c5fea35c7d
Tyler <63419902+TylerAustinW@users.noreply.github.com>
2025-06-18 13:19:21 -0400

    Add Studio Sessions section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/layout/navbar";
 import BiographySection from "@/components/sections/biography-section";
 import HeroSection from "@/components/sections/hero";
 import MusicShowcaseSection from "@/components/sections/MusicShowcaseSection";
+import StudioSessionsSection from "@/components/sections/studio-sessions-section";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <main>
         <HeroSection />
         <MusicShowcaseSection />
+        <StudioSessionsSection />
         <BiographySection />
       </main>
     </div>
